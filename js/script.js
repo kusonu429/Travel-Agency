386b196eb1cbe96af7662ecac7e63ea4f5a0f90b
Sonukumar <98530388+kusonu429@users.noreply.github.com>
2022-04-01 20:52:00 +0530

    done site
    Done Website

--- js/script.js
+++ js/script.js
@@ -47,17 +47,21 @@ $('.owl-carousel').owlCarousel({
   loop:true,
   autoplayTimeout:5000,
   autoplay:true,
-  margin:25,
+  margin:25, 
   nav:false,
   responsive:{
       0:{
           items:1
       },
-      600:{
+      768:{
+          items:2
+      },
+      992:{
           items:3
       },
-      991:{
-          items:3
+
+      1200:{
+        items:3
       }
   }
 })
